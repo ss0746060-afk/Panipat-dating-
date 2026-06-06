5202b68947040fb0a53d13268015e0e019791d32
ss0746060-afk <ss0746060@gmail.com>
2026-06-06 18:35:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,9 @@
-let users = [
-  {name:"Anjali", age:22, bio:"Love food & travel ❤️"},
-  {name:"Priya", age:21, bio:"Coffee + long drives ☕"},
-  {name:"Neha", age:23, bio:"Simple & happy 😊"},
-  {name:"Simran", age:24, bio:"Music lover 🎧"}
-];
-
-function loadProfiles(){
-  let box=document.getElementById("cards");
-  box.innerHTML="";
-
-  users.forEach(u=>{
-    let div=document.createElement("div");
-    div.className="profile-card";
-
-    div.innerHTML=`
-      <h3>${u.name}, ${u.age}</h3>
-      <p>${u.bio}</p>
-      <button onclick="like()">❤️ Like</button>
-      <button onclick="skip()">❌ Skip</button>
-    `;
-
-    box.appendChild(div);
-  });
-}
-
-function like(){
-  alert("💘 Match Found!");
-}
-
-function skip(){
-  alert("Skipped");
-}
-
-function toggleTheme(){
-  document.body.classList.toggle("dark");
+function like(element) {
+    if(element.classList.contains('fa-regular')) {
+        element.classList.replace('fa-regular', 'fa-solid');
+        element.style.color = "#ed4956";
+    } else {
+        element.classList.replace('fa-solid', 'fa-regular');
+        element.style.color = "black";
+    }
 }
