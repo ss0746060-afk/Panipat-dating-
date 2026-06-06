372c3d31b3981773516818e9eba651b47a36aad5
ss0746060-afk <ss0746060@gmail.com>
2026-06-06 18:42:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const feed = document.getElementById('feed');
 
-// पोस्ट का डेटा
-const postData = { user: "Chef_Priya", content: "आज का स्पेशल!" };
+// बड़े लेवल का डेटा
+const posts = [
+    { user: "Priya_Foodie", img: "image1.jpg", likes: "1,200" },
+    { user: "Rahul_Travels", img: "image2.jpg", likes: "890" }
+];
 
-function createPost() {
-    feed.innerHTML = `
-        <div class="post">
-            <div class="post-header">${postData.user}</div>
+function renderPosts() {
+    posts.forEach(post => {
+        const postDiv = document.createElement('div');
+        postDiv.className = 'post-card';
+        postDiv.innerHTML = `
+            <div class="post-header"><b>${post.user}</b></div>
             <div class="post-img"></div>
-            <div class="post-actions">
+            <div class="actions">
                 <i class="fa-regular fa-heart" onclick="this.style.color='red'"></i>
                 <i class="fa-regular fa-comment"></i>
                 <i class="fa-regular fa-paper-plane"></i>
             </div>
-            <p style="padding: 0 10px;"><b>${postData.user}</b> ${postData.content}</p>
-        </div>
-    `;
+            <p style="padding-left:15px; padding-bottom:15px;">${post.likes} Likes</p>
+        `;
+        feed.appendChild(postDiv);
+    });
 }
 
-createPost();
+renderPosts();
