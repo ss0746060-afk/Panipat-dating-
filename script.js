da9c0b4dc50024258d51430e3fd88d4741095d72
ss0746060-afk <ss0746060@gmail.com>
2026-06-06 21:31:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("वेबसाइट लोड हो गई है!");
+function changePage(pageName) {
+    alert("आप " + pageName + " पेज पर हैं!");
+    
+    // यहाँ आप बाद में यह लिख सकते हैं कि कौन सी स्क्रीन दिखानी है
+    const feed = document.getElementById('feed');
+    if(pageName === 'Profile') {
+        feed.innerHTML = "<h1>प्रोफाइल सेक्शन यहाँ दिखेगा</h1>";
+    } else {
+        feed.innerHTML = "<h1>" + pageName + " सेक्शन है</h1>";
+    }
+}
